9c04c7d07bc62aaf8352998b7aae8f7f8b9c643d
Oleg Orlov <mail@orlovoleg.com>
2016-11-02 14:43:49 +0300

    added normalizr export

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+import { Schema, arrayOf, valuesOf, unionOf } from 'normalizr'
+
 import createAction from './lib/createAction'
 import * as reducers from './reducers'
 
 
 export default {
+  Schema,
+  arrayOf,
+  valuesOf,
+  unionOf,
+
   createAction,
   reducers,
 }
